9cceb9c814f7d34e889165e602cdd0038a2d90af
Richard Friedman <rfriedman@themeetgroup.com>
2018-02-11 17:37:52 -0500

    Testing parsing json

--- codeship.sh
+++ codeship.sh
@@ -1,15 +1,17 @@
 
-
-curl https://www.redline13.com/Api/LoadTest \
+echo curl https://www.redline13.com/Api/LoadTest \
   -H "X-Redline-Auth: ${REDLINE_API_KEY}" \
   -F testType=jmeter-test \
   -F name=CodeShipAndRedLine13 \
   -F "file=@`pwd`/tests/Plexify.jmx" \
-  -F numServers=2 \
+  -F numServers=1 \
   -F storeOutput=T \
   -F servers[0][location]=us-east-1 \
   -F servers[0][size]=m3.medium \
-  -F servers[0][num]=2 \
+  -F servers[0][num]=1 \
   -F servers[0][onDemand]=T \
-  -F servers[0][usersPerServer]=1
+  -F servers[0][usersPerServer]=1 
+
+curl https://www.redline13.com/Api/LoadTest?loadTestId=55147 \
+  -H "X-Redline-Auth: ${REDLINE_API_KEY}" | jq -r
 
