b9f1660d02a10912f3ee75c9e76ae8e480b17db5
Richard Friedman <rfriedman@themeetgroup.com>
2018-02-11 19:31:24 -0500

    Made codeship sh more flexible

--- codeship.sh
+++ codeship.sh
@@ -1,5 +1,6 @@
 
-echo curl https://www.redline13.com/Api/LoadTest \
+## Execute Load Test
+OUT=$(curl -vv -s https://www.redline13.com/Api/LoadTest \
   -H "X-Redline-Auth: ${REDLINE_API_KEY}" \
   -F testType=jmeter-test \
   -F name=CodeShipAndRedLine13 \
@@ -10,10 +11,53 @@ echo curl https://www.redline13.com/Api/LoadTest \
   -F servers[0][size]=m3.medium \
   -F servers[0][num]=1 \
   -F servers[0][onDemand]=T \
-  -F servers[0][usersPerServer]=1 
+  -F servers[0][usersPerServer]=1)
+LOADTEST=$(echo $OUT | jq '.loadTestId | tonumber' )
+echo "TRACKING LOAD TEST ($LOADTEST), kill test after 15 minutes"
 
-curl -s https://www.redline13.com/Api/LoadTest?loadTestId=55147 \
-  -H "X-Redline-Auth: ${REDLINE_API_KEY}" | jq '.[0].completed_time,.[0].cancelled_time'
+if [ "$LOADTEST" == "" ]; then
+  echo "LOADTEST FAILED TO CREATE"
+  echo $OUT
+  exit 1
+fi
 
+## Setup End Time to catch long running tests ( 900 seconds )
+secs=900
+endTime=$(( $(date +%s) + secs ))
 
+## Loop until test completes or time is up and kill test.
+COMPLETE=false
+while [ $(date +%s) -lt $endTime -a $COMPLETE == "false" ];
+do
+  sleep 30
+  OUT=$(curl -s https://www.redline13.com/Api/LoadTest?loadTestId=$LOADTEST \
+    -H "X-Redline-Auth: ${REDLINE_API_KEY}")
+  COMPLETE=$( echo $OUT | jq '.[0].completed_time != .[0].cancelled_time')
+  echo "TEST RUNNING"
+done
 
+if [ $COMPLETE == "false" ]; then
+  echo "TEST DID NOT COMPLETE IN TIME"
+  echo "Check test at https://www.redline13.com/LoadTest/View/$LOADTEST"
+  exit 1
+fi
+
+echo "Test Report and Graphs can be viewed https://www.redline13.com/LoadTest/View/$LOADTEST"
+
+## CHECK STATS FOR SUCCESS FAILURE
+SUCCESS_RATE=${SUCCESS_RATE:=0}
+RESPONSE_TIME=${RESPONSE_TIME:=10000}
+RESULT=$(curl -s https://www.redline13.com/Api/LoadTest?loadTestId=$LOADTEST \
+  -H "X-Redline-Auth: ${REDLINE_API_KEY}") 
+PASS=$(echo $RESULT | jq '.[0].success_rate >= '$SUCCESS_RATE' and .[0].avg_resp_time < '$RESPONSE_TIME)
+if [ $PASS != "true" ]; then
+  RATE=$(echo $RESULT | jq '.[0].success_rate')
+  TIME=$(echo $RESULT | jq '.[0].avg_resp_time')
+  echo "TEST DID NOT MEET REQUIREMENTS, FAIL - Check Load Test for detailed results"
+  echo "Expecting SUCCESS RATE $SUCCESS_RATE ACTUAL $RATE"
+  echo "Expecting RESPONSE TIME $RESPONSE_TIME ACTUAL $TIME"
+  echo $RESULT | jq 
+  exit 1
+fi
+
+## TODO show to access report data and check for specific endpoint metrics
